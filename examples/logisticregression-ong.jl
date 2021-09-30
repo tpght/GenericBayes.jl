a246b5e33cbb4d9416601df3aab49ac7bec44a9a
Tom Pennington <tp373@bath.ac.uk>
2021-09-30 11:13:18 +0100

    Add natural orthogonal gradient method

---
+++ examples/logisticregression-ong.jl
@@ -0,0 +1,32 @@
+using GenericBayes, Distributions, LinearAlgebra, StatsFuns, Plots, MCMCChains
+
+p = 2                           # Number of coefficients
+n = 100                         # Number of observations (data)
+
+# Generate design matrix
+X = rand(Normal(), (n, p))
+@show β_true = ones(p)
+μ = StatsFuns.logistic.(X * β_true)
+y = rand(Product(Bernoulli.(μ)))
+
+# Build model
+ρ = 0.99
+Σ = diagm(ones(p))
+Σ[2,1] = ρ
+Σ[1,2] = ρ
+model = CanonicalGLM{Bernoulli, Float64}(X, y, zeros(p), Σ)
+
+N = 1000                        # Number of samples
+subsampler = SphericalRandomWalk(2.38)
+subsamples = Int(1000)
+sampler = OrthogonalNaturalGradient(NegativeLogDensity(), subsampler, subsamples)
+
+samples = sample(model, sampler, N, chain_type=MCMCChains.Chains)
+chain = Chains(samples)
+
+if(p == 2)
+    plot(model, samples, 1.0)
+end
+
+pyplot()
+autocorplot(chain)
---
+++ src/samplers/OrthogonalNaturalGradient.jl
@@ -0,0 +1,96 @@
+import AbstractMCMC.AbstractSampler, AbstractMCMC.step
+export OrthogonalNaturalGradient
+
+"""
+    OrthogonalNaturalGradient
+
+Orthogonal Gibbs, recursing on e-flat submanifolds.
+"""
+struct OrthogonalNaturalGradient <: AbstractSampler
+    geometry::Bregman           # Geometry to be used in the sampler
+    subsampler::AbstractSampler # Sampler to be used on each m-geodesic
+    subsamples::Int             # Number of times to run the embedded sampler
+end
+
+
+"""
+    step(rng, model, sampler,
+              state=nothing; kwargs...)
+
+One iteration of the orthogonal natural gradient method.
+"""
+function step(rng, model::BayesModel, sampler::OrthogonalNaturalGradient,
+              current_state=nothing; kwargs...) where T<:Real
+
+    # First, generate an initial state if required
+    if (current_state == nothing)
+        # NOTE: Can't use the mode for this, because natural gradient is zero at
+        # the mode.
+        state = max_posterior(model, zeros(dimension(model))) + rand(Normal(), dimension(model))
+        return state, state
+    end
+
+    """
+        ONG
+
+    Performs the ONG method by sampling on a k-dimensional m-flat then
+    e-flat submanifold
+    """
+    function ONG(log_density::Function, θ0::Vector{<:Real}, generator::Function)
+        # Get dimension of model
+        p = length(θ0)
+
+        # Check if we're on the last m-geodesic
+        # TODO Can also stop before we get to this point.
+        if(p == 1)
+            # Sample on the remaining l variables (e-flat)
+            samples = AbstractMCMC.sample(rng, LogDensityModel(log_density, 1), sampler.subsampler,
+                   sampler.subsamples, progress=false)
+
+            return samples[end]
+        end
+
+        # Compute the gradient with respect to primal co-ordinates.
+        gradient = ForwardDiff.gradient(log_density, θ0)
+
+        # Convert current point to dual co-ordinates (take gradient of generator)
+        η0 = legendre_dual(θ0, generator)
+
+        # Parameterize the dual-geodesic to sample along
+        dg(t) = η0 + t .* gradient
+
+        # Sample from the dual geodesic η0 + t * gradient
+        function mgeodesic_log_target(t)
+            primal = inverse_legendre_dual(dg(t), generator, x0=θ0)
+            log_density(primal) - logabsdetmetric(primal, generator)
+        end
+
+        # Sample from density defined by mflat_log_target
+        samples = AbstractMCMC.sample(rng,
+                                      LogDensityModel(mgeodesic_log_target, 1),
+                                      sampler.subsampler, sampler.subsamples, progress=false)
+
+        # samples[end] is now the resampled parameter along the dual geodesic.
+        # Convert to a primal co-ordinate.
+        θ1 = inverse_legendre_dual(dg(samples[end]), generator, x0 = θ0)
+
+        # Construct a basis for the space orthogonal to gradient
+        B = (diagm(ones(p)) - (1.0 / norm(gradient, 2)^2) * (gradient * gradient'))[:, 1:(p-1)]
+
+        # Use recursion to sample the remaining k variables.
+        restricted_log_density(x) = log_density(θ1 + B * x)
+        restricted_generator(x) = generator(θ1 + B * x)
+        sample = ONG(restricted_log_density, zeros(p-1), restricted_generator)
+
+        return θ1 + B * sample
+    end
+
+    log_density(x) = log_posterior_density(model, x)
+    generator(x) = bregman_generator(x, sampler.geometry, model)
+
+    # Call the orthogonal gibbs function
+    state = ONG(log_density, current_state, generator)
+
+    # Return new state
+    return state, state
+end
