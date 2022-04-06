e9ad11b569dc9f25da15a17a6103545769890302
Tom Pennington <tp373@bath.ac.uk>
2022-04-06 22:05:46 +0100

    change orthogonal gradient to slice subsampling

--- src/samplers/OrthogonalGradient.jl
+++ src/samplers/OrthogonalGradient.jl
@@ -1,17 +1,31 @@
 import AbstractMCMC.AbstractSampler, AbstractMCMC.step
 export OrthogonalGradient
 
+
+function log_cond_density(α, r, c, δ, model, Ap, geometry, x0)
+    b = α .* r  + c
+
+    if(Ap == Float64[] || size(Ap)[2] == 0)
+        return log_posterior_density(model,b), b
+    end
+
+    θ = inverse_legendre_dual(δ[1:size(Ap)[2]], b, Ap, geometry, model; x0 = x0)
+    log_dens = log_posterior_density(model, θ) - logabsdetmetric(θ, geometry, model, Ap)
+    return log_dens, θ
+end
+
 """
     OrthogonalGradient
 
 Orthogonal Gradient sampler
 """
 struct OrthogonalGradient{T<:Real} <: AbstractSampler
+    initial_θ::Vector{T}        # Initial point at which to start sampler
     geometry::Bregman           # Geometry to be used in the sampler
-    proposal::Proposal
-    nsubsamples::Int                  # Number of times to run the embedded sampler
-    initial_θ::Vector{T}
-    stop::Int
+    w::T                        # Initial window size for slice subsampling
+    m::Int                      # Maximum number of times to stepout (slice sampler)
+    nsubsamples::Int            # Number of times to run the embedded sampler
+    stop::Int                   # Number of steps after which to restart the sampler.
 end
 
 """
@@ -62,48 +76,76 @@ function step(rng, model::BayesModel, sampler::OrthogonalGradient,
             return θ, θ
         end
 
-        # scale = 1.0 / prec
-
+        # Now sample from the relevant conditional (see thesis)
+        # For this we use slice sampling (see Neal's paper).
+        # TODO make these parameters of the algorithm.
         α = α0
 
+        # Evaluate log density of the conditional.
+        logf = log_posterior_density(model, θ) -
+               logabsdetmetric(θ, sampler.geometry, model, Ap)
+
         for i=1:sampler.nsubsamples
+            # TODO pinv can be simplified; A has orthogonal columns
+            x0 = pinv(Ap) * θ
 
-            scale = 1.0 / sqrt(r' * metric(θ, sampler.geometry, model) * r)
-            proposal = RandomWalkProposal(scale)
-            # Generate a proposal for α using α0 as initial point
-            # α_prop = draw_proposal(rng, sampler.proposal, [α])[1]
-            # α_prop = rand(rng, Normal(α, scale))
-            α_prop = draw_proposal(rng, proposal, [α])[1]
+            # Firstly sample y ~ Unif(0, f(α))
+            # Equivalently to sampling z = g(α) - e
+            # where g = log(f) and e ~ Exponential(1).
+            z = logf - rand(rng, Exponential(1.0))
 
-            # Embed the proposed alpha value
-            # TODO: pinv here can be speeded up (Ap has orthogonal columns)
-            b = α_prop .* r  + c
-            θ_prop = inverse_legendre_dual(δ[1:(j-1)], b, Ap, sampler.geometry,
-            model; x0 = pinv(Ap) * θ)
+            # Slice is now defined by S = {x: z < g(x)}
 
-            # Evaluate density at current and proposed points
-            # α0 should be embedded to current θ
-            log_dens_prop = log_posterior_density(model, θ_prop) -
-                logabsdetmetric(θ_prop, sampler.geometry, model, Ap)
-            log_dens_current = log_posterior_density(model, θ) -
-                logabsdetmetric(θ, sampler.geometry, model, Ap)
+            # Stepping out procedure. (Fig 3 in Neal)
+            # TODO move to separate function
+            U = rand(rng, Uniform())
+            L = α - sampler.w * U
+            R = L + sampler.w
+            V = rand(rng, Uniform())
+            J = floor(sampler.m * V)
+            K = (sampler.m-1)-J
 
-            # Evaluate log-ratio of conditional proposal densities
-            # logprop_ratio = log_proposal_ratio(sampler.proposal, θ_prop, θ)
-            logprop_ratio = log_proposal_ratio(proposal, θ_prop, θ)
-            # logprop_ratio = 0.0
-
-            # Evaluate metropolis-hastings log-ratio
-            logp_accept = log_dens_prop - log_dens_current + logprop_ratio
-            # Accept/reject
-            if(logp_accept >= 0 || log(rand(rng)) < logp_accept)
-                # Accept
-                θ = θ_prop
-                α = α_prop
-                continue
+            logfL, θ_L = log_cond_density(L, r, c, δ, model, Ap, sampler.geometry, x0)
+            while (J >0 && logfL > z)
+                L = L -sampler.w
+                J = J-1
+                logfL, θ_L = log_cond_density(L, r, c, δ, model, Ap, sampler.geometry, x0)
             end
 
-            # If we've reached here, rejected. Keep θ the same.
+            logfR, θ_R = log_cond_density(R, r, c, δ, model, Ap, sampler.geometry, x0)
+            while (K >0 && logfR > z)
+                R = R +sampler.w
+                K = K-1
+                logfR, θ_R = log_cond_density(R, r, c, δ, model, Ap, sampler.geometry, x0)
+            end
+
+            # Shrinkage and sampling procedure.
+            Lb = L
+            Rb = R
+
+            while (true)
+                U = rand(rng, Uniform())
+                α1 = Lb + U *(Rb - Lb)
+                logfα1, θ_α1 = log_cond_density(α1, r, c, δ, model, Ap, sampler.geometry, x0)
+                if(z < logfα1)
+                    # Accept!
+                    α = α1
+                    logf = logfα1
+                    θ[:] .= θ_α1[:]
+                    break
+                end
+
+                if(α1 < α)
+                    Lb = α1
+                else
+                    Rb = α1
+                end
+
+                if(Lb ≈ Rb)
+                    @error "Interval shrunk to 0"
+                end
+            end
+
         end
 
         # Compute gradient of Bregman generator
